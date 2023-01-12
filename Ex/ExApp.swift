1b82f9ac00914343f9442ecbf74ba8c9ef8442b6
ocobanoglu18 <ocobanoglu18@ku.edu.tr>
2023-01-12 12:18:18 +0300

    Initial Commit

---
+++ Ex/ExApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExApp.swift
+//  Ex
+//
+//  Created by Adnan Cobanoglu on 12.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
